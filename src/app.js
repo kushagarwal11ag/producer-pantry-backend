25595afd705e127970faf9411ae1623f6aa197a5
Kushal <kushagarwal11ag@gmail.com>
2024-04-10 13:47:18 +0530

    added client errors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import ApiError from "./utils/ApiError.js";
 
 const app = express();
 
@@ -21,10 +22,27 @@ app.use(cookieParser());
 
 // routes import
 import userRouter from "./routes/user.routes.js";
-import cropRouter from "./routes/crop.routes.js"
+import cropRouter from "./routes/crop.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/crops", cropRouter);
+
+app.use((err, req, res, next) => {
+	if (err instanceof ApiError) {
+		// Custom ApiError handling
+		res.status(err.statusCode).json({
+			success: false,
+			message: err.message,
+			errors: err.errors, // Optional, if you're using the errors array
+		});
+	} else {
+		// Generic error handling
+		res.status(500).json({
+			success: false,
+			message: "Internal Server Error",
+		});
+	}
+});
 
 export default app;
